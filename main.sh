2f6aa3964294e35e058cb4ce0985edd19fe73c03
Aissatou Barry <115582067+aissatoubarry938@users.noreply.github.com>
2023-02-23 17:51:34 +0000

    Initial commit

---
+++ main.sh
@@ -0,0 +1,6 @@
+echo "How are you today?"
+read input
+echo "I am glad you are $input"
+echo "what is your favorite ice cream?"
+read icecream
+echo "$icecream, wow mine is chocolate chip! Lets go get some."
